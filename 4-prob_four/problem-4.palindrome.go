2d4781c9bb9c08fea8d4ecd3e06909a6ca21f6c1
Troy Kelley <trkelley28@gmail.com>
2025-07-16 16:03:31 -0400

    clean up

--- 4-prob_four/problem-4.palindrome.go
+++ 4-prob_four/problem-4.palindrome.go
@@ -6,52 +6,7 @@ package prob_four
 
 // Find the largest palindrome made from the product of two
 // 3-digit numbers.
-/**
-	// The largest number that can be made from the product of two N-digit numbers is
-	// 9_n ^2
-	// the smallest is 10_n^2
-	// so for N=3 the answer A lies within 100^2 < A < 999^2 = 10,000 < A < 998,001
-	// the answer cannot be prime because then its only factors would be 1 and itself, and since 1 is not a N digit number (for N > 1)
 
-	// the closest palindrome number to 998,001 is 997,799
-	=
-				  9
-			   90
-			  700
-		  7,000
-		 90,000
-	+ 900,000
-
-	996699
-	995599
-	994499
-	993399
-	992299
-	991199
-	990099
-	989989
-	...
-	11211
-	11111
-	11011
-	...
-	10301
-	10201
-	10101
-	10001
-
-	9 * 1 = 9
-	= (9 * 1) * (1)
-	91 * 99 =
-	(1 * 99) + (90 * 99) =
-  (1 * ((9 * 1) + (9 * 10))) + ((9 * 10) * ((9 * 1) + (9 * 10))) =
-	((9 * 1) + (9 * 10)) + ((9 * 10) * (9 * 1)) + ((9 * 10) * (9 * 10)) =
-	((9 * 1) + (9 * 10)) + (9 * 9 * 10) + (9 * 9 * 10 * 10) =
-	((9^1 * 10^0) + (9^1 * 10^1)) + (9^2 * 10^1) + (9^2 * 10^2)
-
-	LHS = (9 * 1) + (9 * 10) + (9 * 100) = 999
-	RHS =
-*/
 import (
 	"fmt"
 	"strconv"
@@ -82,7 +37,6 @@ func LargestPalindrome(n int) int {
 			product := int(i * j)
 			if isPalindrome(product) && product > answer {
 				answer = product
-				fmt.Printf("%v is a palidrome number made by factors %v and %v\n", product, i, j)
 				break
 			}
 		}
--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	prob_four "github.com/trdancer/projeuler/4-prob_four"
-)
-
 func main() {
-	for i := range 9 {
-		prob_four.LargestPalindrome(i + 1)
 
-	}
 }
